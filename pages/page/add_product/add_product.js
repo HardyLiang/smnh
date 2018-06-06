a3937c1a2a18c9991f66fd3b2cff416b5440b287
Arealy <2466728596@qq.com>
2018-06-06 16:59:41 +0800

    pubish goods modify

--- pages/page/add_product/add_product.js
+++ pages/page/add_product/add_product.js
@@ -1,15 +1,15 @@
 
-var event =require('../../../utils/event.js')
+var event = require('../../../utils/event.js')
 Page({
   data: {
     showView: false,  //是否分销
 
     imageList: [],
-    chooseGoods:"", //显示用户选择的产品类型，
-    chosseGoodsId:"",//存放用户选择的产品的Id
- 
+    chooseGoods: "", //显示用户选择的产品类型，
+    chosseGoodsId: "",//存放用户选择的产品的Id
+
   },
-  
+
   onLoad: function () {
     // 生命周期函数--监听页面加载
     var showView = true;
@@ -27,32 +27,32 @@ Page({
     var that = this,
       imageList = this.data.imageList;
     console.log(imageList.length)
-    if (imageList.length <= 4){ 
-    wx.chooseImage({
-      count: 5 - imageList.length, // 最多可以选择的图片张数，默认9
-      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
-      success: function (res) {
-        // const src = res.tempFilePaths[0]
-        var imgsrc = res.tempFilePaths;
-        // wx.navigateTo({
-        //   url: `../upload/upload?imgsrc=${imgsrc}`
-        // })
-        // var imgsrc = res.tempFilePaths;
-        imageList = imageList.concat(imgsrc);
-        that.setData({
-          imageList: imageList
-        });
-      },
-      fail: function () {
-        // fail
-      },
-      complete: function () {
-        // complete
-      }
-    })
-    return;
-    }else{
+    if (imageList.length <= 4) {
+      wx.chooseImage({
+        count: 5 - imageList.length, // 最多可以选择的图片张数，默认9
+        sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+        success: function (res) {
+          // const src = res.tempFilePaths[0]
+          var imgsrc = res.tempFilePaths;
+          // wx.navigateTo({
+          //   url: `../upload/upload?imgsrc=${imgsrc}`
+          // })
+          // var imgsrc = res.tempFilePaths;
+          imageList = imageList.concat(imgsrc);
+          that.setData({
+            imageList: imageList
+          });
+        },
+        fail: function () {
+          // fail
+        },
+        complete: function () {
+          // complete
+        }
+      })
+      return;
+    } else {
       wx.showToast({
         icon: 'none',
         title: "最多只能上传5张哦~"
@@ -67,16 +67,32 @@ Page({
       urls: this.data.imageList
     })
   },
-/**
- * 用户点击跳转选择产品页面
- */
+  /**
+   * 用户点击跳转选择产品页面
+   */
   onProductClassify: function (event) {
     wx.navigateTo({
       url: "product_classify/product_classify"
     })
   },
-  onShow:function(){
-    var that =this;
+  /**
+ * 用户点击跳转产品主图
+ */
+  onProductImg: function (event) {
+    wx.navigateTo({
+      url: "product_img/product_img"
+    })
+  },
+  /**
+ * 用户点击跳转产品详情页
+ */
+  onProductDetail: function (event) {
+    wx.navigateTo({
+      url: "product_detail/product_detail"
+    })
+  },
+  onShow: function () {
+    var that = this;
     //获取事件传递信息
     event.on(event.KChooseGoodItemSuccessEventName, this, function (data) {
       console.log("用户选完了产品了。我收到了");
@@ -92,7 +108,7 @@ Page({
     //页面销毁清除页面event接收事件
     event.remove(event.KChooseGoodItemSuccessEventName, this);
   },
-  
+
 
 
 })
---
+++ pages/page/add_product/product_img/product_img.js
@@ -0,0 +1,80 @@
+Page({
+  data: {
+    imgUrlValue: "",
+    imageList: [],
+
+  },
+
+  onLoad: function () {
+
+  },
+  /**
+  * 上传主图 
+  */
+  uploadImg: function () {
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success(res) {
+        const src = res.tempFilePaths[0]
+        wx.navigateTo({
+          url: `../../upload/upload?src=${src}`
+        })
+      }
+    })
+  },
+  // 上传其他图片，没规定大小
+  chooseImage: function () {//这里是选取图片的方法
+    var that = this,
+      imageList = this.data.imageList;
+    // console.log(imageList.length)
+    if (imageList.length <= 3) {
+      wx.chooseImage({
+        count: 4 - imageList.length, // 最多可以选择的图片张数，默认9
+        sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+        success: function (res) {
+          var imgsrc = res.tempFilePaths;
+          imageList = imageList.concat(imgsrc);
+          that.setData({
+            imageList: imageList
+          });
+        },
+        fail: function () {
+          // fail
+        },
+        complete: function () {
+          // complete
+        }
+      })
+      return;
+    } else {
+      wx.showToast({
+        icon: 'none',
+        title: "一共最多只能上传5张哦~"
+      })
+    }
+  },
+  // 预览图片
+  previewImage: function (e) {
+    var imageList = e.currentTarget.dataset.index;
+    var imageList = this.data.imageList;
+    wx.previewImage({
+      current: imgs[index],
+      urls: this.data.imageList
+    })
+  },
+  // 删除图片
+  deleteImg: function (e) {
+    var imageList = this.data.imageList;
+    var index = e.currentTarget.dataset.index;
+    imageList.splice(index, 1);
+    this.setData({
+      imageList: imageList
+    });
+  },
+
+
+
+})
